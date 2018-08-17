da9d961dc0fc38ca559993a62e9e6ae4a5476a56
Jenn Lao <jenn.p.lao@gmail.com>
2018-08-16 23:53:34 -0600

    Update refactored code

--- lib/event.rb
+++ lib/event.rb
@@ -17,39 +17,56 @@ class Event
 
   def average_age
     sum = ages.sum
-    count = ages.count
-    sum/count.to_f.round(2)
+    number = ages.count
+    sum/number.to_f.round(2)
   end
 
   def standard_deviation_age
-    sum = ages.sum
-    count = ages.count
-    average = sum/count.round(1)
+    ages_total    = summed_ages
+    age_differences  = average_differences(ages)
+    squares       = squared_differences(age_differences)
+    squared_sum   = squares_total(squares)
+    divided_total = divided_sum(squared_sum)
+    Math.sqrt(divided_total).round(2)
+  end
 
-    differences = []
+  def summed_ages
+    ages_total = 0
     ages.each do |age|
-      difference = age - average
-      differences << difference.round(1)
+      ages_total += age
     end
-    differences
+    ages_total
+  end
 
-    squares = []
-    differences.each do |difference|
-      square = difference**2
-      squares << square.round(2)
+  def average_differences(ages)
+    age_differences = []
+    ages.each do |age|
+      difference = age - average_age
+      age_differences << difference.round(1)
+    end
+    age_differences
+  end
+
+  def squared_differences(age_differences)
+    squares= []
+    age_differences.each do |difference|
+      squared = difference**2
+      squares << squared.round(2)
     end
     squares
+  end
 
+  def squares_total(squares)
     squared_sum = 0
     squares.each do |square|
       squared_sum += square
     end
-
     squared_sum.round(1)
-
-    divided_sum = squared_sum/count
-    divided_sum.round(2)
-
-    Math.sqrt(divided_sum).round(2)
   end
+
+  def divided_sum(squared_sum)
+    divided_total = squared_sum / (ages.count)
+    divided_total.round(2)
+  end
+
 end
--- test/event_test.rb
+++ test/event_test.rb
@@ -52,5 +52,40 @@ class EventTest < Minitest::Test
     assert_equal expected, actual
   end
 
+  def test_it_can_get_summed_ages
+    event = Event.new("Curling", [24, 30, 18, 20, 41])
+    expected = 133
+    actual = event.summed_ages
+    assert_equal expected, actual
+  end
+
+  def test_it_can_get_average_differences
+    event = Event.new("Curling", [24, 30, 18, 20, 41])
+    expected = [-2.6, 3.4, -8.6, -6.6, 14.4]
+    actual = event.average_differences([24, 30, 18, 20, 41])
+    assert_equal expected, actual
+  end
+
+  def test_it_can_get_squared_differences
+    event = Event.new("Curling", [24, 30, 18, 20, 41])
+    expected = [6.76, 11.56, 73.96, 43.56, 207.36]
+    actual = event.squared_differences([-2.6, 3.4, -8.6, -6.6, 14.4])
+    assert_equal expected, actual
+  end
+
+  def test_it_can_get_squares_total
+    event = Event.new("Curling", [24, 30, 18, 20, 41])
+    expected = 343.2
+    actual = event.squares_total([6.76, 11.56, 73.96, 43.56, 207.38])
+    assert_equal expected, actual
+  end
+
+  def test_it_can_get_divided_sum
+
+    event = Event.new("Curling", [24, 30, 18, 20, 41])
+    expected = 68.64
+    actual = event.divided_sum(343.2)
+    assert_equal expected, actual
+  end
 
 end
